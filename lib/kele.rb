eeec4956d16bae7e506c48ef5827671bff70be40
Kira Helm <soulclimberchick@gmail.com>
2018-12-12 14:26:13 -0800

    Checkpoint 5 Roadmap and Checkpoints completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,30 +1,31 @@
-require './lib/kele'
 require 'httparty'
 require 'json'
+require_relative 'roadmap'
 
 class Kele
   include HTTParty
-  base_uri 'https://www.bloc.io/api/v1'
+	include Roadmap
+
+ 	base_uri "https://www.bloc.io/api/v1/"
 
   def initialize(email, password)
-    response = self.class.post('/sessions', body: { email: email, password: password })
+ 		response = self.class.post(base_api_url("sessions"), body: { "email": email, "password": password })
+ 		raise "Invalid email or password" if response.code == 404
+ 		@auth_token = response["auth_token"]
+ 	end
 
-    @auth = response['auth_token']
-    if @auth == nil
-      puts 'Invalid Credentials'
-    else
-      puts 'Welcome'
-    end
-  end
+ 	def get_me
+ 		response = self.class.get(base_api_url("users/me"), headers: { "authorization" => @auth_token })
+ 		JSON.parse(response.body)
+ 	end
+ 
+ 	def get_mentor_availability(id)
+ 		response = self.class.get(base_api_url("mentors/#{id}/student_availability"), headers: { "authorization" => @auth_token })
+ 		JSON.parse(response.body)
+ 	end
 
-  def get_me
-    response = self.class.get('/users/me', headers: { "authorization" => @auth })
-    JSON.parse(response.body)
-  end
-
-  def get_mentor_availability(mentor_id)
-    mentor_url = "https://www.bloc.io/api/v1/mentors/#{mentor_id}/student_availability"
-    response = self.class.get(mentor_url, headers: {"content_type" => "application/json", "authorization" => @auth_token})
-    body = JSON.parse(response.body)
-  end
-end
+ 	private
+ 	def base_api_url(endpoint)
+ 		"https://www.bloc.io/api/v1/#{endpoint}"
+ 	end
+ end
